917c6183d70dc6d861e6969b3146d5772af696db
sajood ali <sajoodali486@gmail.com>
2025-12-11 19:41:12 +0500

    feat: Initialize backend with Express server, MongoDB connection, and user authentication endpoints.

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,14 +7,14 @@ import authRoute from './routes/auth.route.js';
 import adminRoute from './routes/admin.route.js';
 
 dotenv.config();
-app.use(cookieParser()); 
-
 connectDB();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+app.use(cookieParser()); 
 app.use(cors());
 app.use(express.json());
 
